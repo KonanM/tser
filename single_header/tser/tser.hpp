11d0899a9d31eae78ddae766be7881e6458731ab
Kinan <kinan.mahdi@gmail.com>
2021-02-04 16:27:07 +0100

    Fix clang conversion warnings

--- single_header/tser/tser.hpp
+++ single_header/tser/tser.hpp
@@ -43,13 +43,13 @@ namespace tser {
 namespace tser {
     //tables for the base64 conversions
     static constexpr auto g_encodingTable = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-    static constexpr auto g_decodingTable = []() { std::array<unsigned char, 256> decTable{}; for (unsigned char i = 0; i < 64u; ++i) decTable[g_encodingTable[i]] = i; return decTable; }();
+    static constexpr auto g_decodingTable = []() { std::array<unsigned char, 256> decTable{}; for (unsigned char i = 0; i < 64u; ++i) decTable[static_cast<unsigned char>(g_encodingTable[i])] = i; return decTable; }();
     static std::string encode_base64(std::string_view in) {
         std::string out;
         unsigned val = 0;
         int valb = -6;
-        for (unsigned char c : in) {
-            val = (val << 8) + c;
+        for (char c : in) {
+            val = (val << 8) + static_cast<unsigned char>(c);
             valb += 8;
             while (valb >= 0) {
                 out.push_back(g_encodingTable[(val >> valb) & 63u]);
@@ -63,8 +63,8 @@ namespace tser {
         std::string out;
         unsigned val = 0;
         int valb = -8;
-        for (unsigned char c : in) {
-            val = (val << 6) + g_decodingTable[c];
+        for (char c : in) {
+            val = (val << 6) + g_decodingTable[static_cast<unsigned char>(c)];
             valb += 6;
             if (valb >= 0) {
                 out.push_back(char((val >> valb) & 0xFF));
